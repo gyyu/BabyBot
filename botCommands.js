0183a9673b6499506efdd118feac5a8fa879d08c
Juan Rios <juan.rios.28@gmail.com>
2018-07-05 12:16:09 -0400

    Bad words logic
    Logic for looking at bad words

--- botCommands.js
+++ botCommands.js
@@ -1,9 +1,10 @@
 let knownCommands = { introduce, feed, teach }
 var timers = [300000, 600000, 900000]
 var randomTime = timers[Math.floor(Math.random() * myArray.length)];
+
 var learnedCurseWords = []
 
-// pulling list of curse words from a text file and putting them into an array
+// Pulling list of curse words from a text file and putting them into an array
 var possibleCurseWords = require('fs').readFileSync(listofbadwords.txt, 'utf-8')
     .split('\n')
     .filter(Boolean);
@@ -18,34 +19,19 @@ client.on('connected', function(addr,port) {
   client.say("user can go here", "Hi I am blank bot here to blank blank")
 });
 
-// What to do if a command is detected
-client.on('chat', function(channel, user, message, self) {
-  if(message === "you suck!")
-   sendMessage('channel', 'message', 'you suck too')
-});
-
-
-// Tags user that it is responding to when it reads the message hi
-client.on('chat', function(channel, user, message, self) {
-  if(message === "Hi")
-    client.say("channel", user['display-name'] + "Hi")
-});
-
-// what to do if the introduce command is detected, 2 ways to do it.
-function introduce (target, context) {
-  sendMessage('channel', 'message', 'introduce blank i am blank intro blank')
-  client.say("user can go here", "introduce blank i am blank intr blank")
+// Bot will do something every 5 mins (in milliseconds), we should probably make a list of possible intervals and rotate through them
+function feed (channel, user, message, self) {
+  setInterval( ()=> {
+    client.say("channel", "Feed me");
+  }, timers);
 }
 
 // Bot will do something every 5 mins (in milliseconds), we should probably make a list of possible intervals and rotate through them
-setInterval( ()=> {
-  client.say("channel", "Feed me");
-}, timers);
-
-// Bot will do something every 5 mins (in milliseconds), we should probably make a list of possible intervals and rotate through them
-setInterval( ()=> {
-  client.say("channel", "I am going to sleep now. Please don't yell or I will start crying");
-}, timers);
+function feed (channel, user, message, self) {
+  setInterval( ()=> {
+    client.say("channel", "I am going to sleep now. Please don't yell or I will start crying");
+  }, timers);
+}
 
 // Helper function to send the correct type of message:
 function sendMessage (target, context, message) {
@@ -80,12 +66,24 @@ function onMessageHandler (target, context, msg, self) {
     // Then call the command with parameters:
     command(target, context, params)
     console.log(`* Executed ${commandName} command for ${context.username}`)
+   
+    // No command was said, but the bot was tagged, we should put whatever is said into a knowledge database
   } else if (commandName === username) 
   {
-    // No command was said, but the bot was tagged do something here
+    if (parse.includes('say')) {
+      sendMessage('channel', 'message', msg)
+      if (parse.some(r=> possibleCurseWords.indexOf(r) >= 0) === true && !(parse.some(r=> learnedCurseWords.indexOf(r) >= 0)))
+      {
+        learnedCurseWords.push(parse)
+      }
+    
+    if (params in possibleCurseWords) {
+      learnedCurseWords.push(params)
   } else {
     console.log(`* Unknown command ${commandName} from ${context.username}`)
   }
+}
+}
 }
 
 
