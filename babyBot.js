8649f69a9e195c9aed550063643b34a5968de86c
ihwang1 <irishwang0104@gmail.com>
2018-07-06 20:14:30 -0400

    1. Refine age calculation 2. Add context to store messages 3. Scan for keywords from chat

--- babyBot.js
+++ babyBot.js
@@ -5,35 +5,57 @@ const setting = require('./Settings/settings.json')
 const opts = setting.opts
 const botState = require('./Settings/botState.json')
 
+let startTime
+let beginingAge = botState.age
+let currentAge
 
-let startTime;
-let currentAge = 0;
 let knownCommands = { "growthReport" : growthReport }
 
 //Function called when !growthReport command is called
 function growthReport (channel, context){
 
-
-  console.log(currentAge)
-
-  reportMessage = "I'm " + currentAge + " years old!"
+  
+  let ageYMD = getAgeInYMD()
+  
+  reportMessage = "I'm " + ageYMD[0] + "-year " + ageYMD[1] + "-month and " + ageYMD[2] + "-day old!"
 
   sendMessage(channel, context, reportMessage)
+
+}
+
+function getAgeInYMD(){
+
+  let currentAgeInYear = Math.floor(currentAge / 720)
+  let remainMonth = currentAge - currentAgeInYear * 720
+
+  let currentAgeInMonth = Math.floor(remainMonth / 30)
+  let remainDay = remainMonth - currentAgeInMonth * 30
+
+  let currentAgeInDay = Math.floor((remainDay % 30))
+
+  console.log(currentAgeInYear + " " + currentAgeInMonth + " " + currentAgeInDay)
+
+  return [currentAgeInYear, currentAgeInMonth, currentAgeInDay]
 
 }
 
 
 function updateAge(){
 
-  currentAge = calculateAge()
+  currentAge = beginingAge + calculateAge()
+  console.log(currentAge)
 }
+
 //Calculates the bot's age
 function calculateAge(){
 
     currentTime = (new Date).getTime()
-    ageInSeconds =  currentTime - startTime
-    convertedAge = ageInSeconds / setting.ageConversion
-    return convertedAge.toFixed(2)
+    ageInSeconds =  (currentTime - startTime) / 1000
+    ageInDay = Math.round(ageInSeconds / 2)
+    console.log("Age in Day " + ageInDay)
+
+    
+    return ageInDay
 }
 
 function checkIfOffLine() {
@@ -65,12 +87,23 @@ function saveAndExit(){
   setTimeout(function() {
 
     botStateRecorder.backupState()
+    updateAge()
     botState.age = currentAge
     botStateRecorder.saveState(botState)
     chatRecorder.saveMsg()
     process.exit(1)
 
   }, setting.exitTimeout)
+
+}
+
+function checkKeywords(keyword,msg){
+
+  if(msg.includes(keyword)){
+
+    sendMessage(opts.channels[0], opts, setting.responses[keyword])
+
+  }
 
 }
 
@@ -112,35 +145,47 @@ function onJoinHandler(target, username,self){
 // Called every time a message comes in:
 function onMessageHandler (target, context, msg, self) {
   
-  let chatMsg = `[${target} (${context['message-type']})] ${context.username}: ${msg}`
+  let chatMsg = `[${target} (${context['message-type']})] ${context.username}: ${msg}` + JSON.stringify(context)
   chatRecorder.storeMsg(chatMsg)
   console.log(chatMsg)
+  //console.log(context)
 
   // Ignore messages from the bot or messages that are not commands
-  if (self || msg.substr(0, 1) !== setting.commandPrefix) {return} 
-
-  // Split the message into individual words:
-  const parse = msg.slice(1).split(' ')
-  // The command name is the first (0th) one:
-  const commandName = parse[0]
-  // The rest (if any) are the parameters:
-  const params = parse.splice(1)
+  if (self) {return} 
   
-  // If the command is known, let's execute it:
-  if (knownCommands[commandName]) {
+  if (msg.substr(0, 1) !== setting.commandPrefix) {
+   
     
-    //get function 
-    const command = knownCommands[commandName]
-    command(target, context, params)
+    setting.keywords.forEach(function(keyword){
 
-    console.log(`* Executed ${commandName} command for ${context.username}`)
-  
-  } else {
+      checkKeywords(keyword,msg)
+    })
+
+  }else{
+
+    // Split the message into individual words:
+    const parse = msg.slice(1).split(' ')
+    // The command name is the first (0th) one:
+    const commandName = parse[0]
+    // The rest (if any) are the parameters:
+    const params = parse.splice(1)
     
-    sendMessage(target, context, setting.noCommandFoundMsg)
-    console.log(`* Unknown command ${commandName} from ${context.username}`)
-  
-  }
+    // If the command is known, let's execute it:
+    if (knownCommands[commandName]) {
+      
+      //get function 
+      const command = knownCommands[commandName]
+      command(target, context, params)
+
+      console.log(`* Executed ${commandName} command for ${context.username}`)
+    
+    } else {
+      
+      sendMessage(target, context, setting.noCommandFoundMsg)
+      console.log(`* Unknown command ${commandName} from ${context.username}`)
+    
+    }
+  } 
 }
 
 // Called every time the bot connects to Twitch chat:
@@ -150,7 +195,7 @@ function onConnectedHandler (addr, port, self) {
 
   
   startTime = (new Date).getTime()
-  console.log(startTime)
+  
 
   setInterval(checkIfOffLine, setting.checkOfflineInterval)
   setInterval(updateAge, setting.checkAgeInterval)
